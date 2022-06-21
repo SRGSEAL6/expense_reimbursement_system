a9f22422b573cee7a296f4e2ef3c6cb53b2b4eae
Genevieve982 <scholler.genevieve@gmail.com>
2022-06-21 10:05:28 -0500

    Register Employee Servlet, needs a little more changes

--- src/main/java/Web/RegisterEmployeeServlet.java
+++ src/main/java/Web/RegisterEmployeeServlet.java
@@ -16,6 +16,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 
 @WebServlet(urlPatterns = {"/register"})
 public class RegisterEmployeeServlet extends HttpServlet {
@@ -26,21 +28,50 @@ public class RegisterEmployeeServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        String emp_ID = req.getParameter("emp_ID");
         String emp_Name = req.getParameter("emp_Name");
-    //    Position emp_Position = req.Enum("emp_Position");
+        Position emp_Position = Position.valueOf(req.getParameter("emp_Position"));
         String username = req.getParameter("username");
         String password = req.getParameter("password");
 
         Employee employee = new Employee();
-        employee.setEmp_ID(emp_ID);
         employee.setEmp_Name(emp_Name);
-    //   employee.setEmp_Position(emp_Position);
+        employee.setEmp_Position(Position.EMPLOYEE);
         employee.setUsername(username);
         employee.setPassword(password);
 
         employeeRepository.save(employee);
         resp.sendRedirect("EmployeeRegistrationForm.html");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        HttpSession session = req.getSession();
+        System.out.println(session.isNew());
+        System.out.println(session.getEmp_Id);
+        System.out.println(new Date(session.getCreationTime()));
+        System.out.println(new Date(session.getLastAccessedTime()));
+
+        String currentUser = (String) session.getAttribute("current-user");
+        System.out.println(currentUser);
+
+        if(currentUser != null) {
+            ring reURI = req.getRequestURI();
+            // read
+            if (reURI.equals("/revature.home.project1/EmployeeRegisterForm.html")) {
+                List<Employee> employees = employeeRepository.findAll();
+                req.setAttribute("all-employees", employees);
+                req.getRequestDispatcher("View_All_Employees.jsp").forward(req, resp);
+            }
+            if (reURI.equals("/pm/delete-product")) {
+                String id = req.getParameter("id");
+                employeeRepository.delete(Integer.parseString(id));
+                resp.sendRedirect("products");
+            }
+        }else{
+            resp.sendRedirect("login.html");
+        }
     }
 
 }
